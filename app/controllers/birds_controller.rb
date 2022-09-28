e7410f7e62b6d325230848aa2e88c126dd39c4b4
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-09-28 11:19:03 +0300

    Completed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,50 +1,56 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+ rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
-  # GET /birds
-  def index
-    birds = Bird.all
-    render json: birds
-  end
+ # GET /birds
+ def index
+   birds = Bird.all
+   render json: birds
+ end
 
-  # POST /birds
-  def create
-    bird = Bird.create(bird_params)
-    render json: bird, status: :created
-  end
+ # POST /birds
+ def create
+   bird = Bird.create!(bird_params)
+   render json: bird, status: :created
+ rescue ActiveRecord::RecordINvalid => invalid 
+     render json: { errors: bird.errors.full_messages }, status: :unprocessable_entity
+   end  
+ end
 
-  # GET /birds/:id
-  def show
-    bird = find_bird
-    render json: bird
-  end
+ # GET /birds/:id
+ def show
+   bird = find_bird
+   render json: bird
+ end
 
-  # PATCH /birds/:id
-  def update
-    bird = find_bird
-    bird.update(bird_params)
-    render json: bird
-  end
+ # PATCH /birds/:id
+ def update
+   bird = find_bird
+   bird.update!(bird_params)
+   render json: bird
+  rescue ActiveRecord::RecordInvalid => invalid
+   render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity 
+ end
 
-  # DELETE /birds/:id
-  def destroy
-    bird = find_bird
-    bird.destroy
-    head :no_content
-  end
+ # DELETE /birds/:id
+ def destroy
+   bird = find_bird
+   bird.destroy
+   head :no_content
+ end
 
-  private
+ private
 
-  def find_bird
-    Bird.find(params[:id])
-  end
+ def find_bird
+   Bird.find(params[:id])
+ end
 
-  def bird_params
-    params.permit(:name, :species, :likes)
-  end
+ def bird_params
+   params.permit(:name, :species, :likes)
+ end
 
-  def render_not_found_response
-    render json: { error: "Bird not found" }, status: :not_found
-  end
+ def render_not_found_response
+   render json: { error: "Bird not found" }, status: :not_found
+ end
 
 end
